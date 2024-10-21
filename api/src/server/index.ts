98db2a0a933eedda0caa9c483fa91a697706a061
sohamhalder <haldersoham46@gmail.com>
2024-10-21 21:09:47 +0530

    abc1

--- api/src/server/index.ts
+++ api/src/server/index.ts
@@ -1,6 +1,10 @@
 import express, { type Request, type Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser"
+import dotenv from "dotenv"
+
+dotenv.config({ path: `${process.cwd()}/.env` });
+
 
 const app = express();
 
@@ -8,4 +12,4 @@ app.get('/', (req:Request, res:Response)=>{
     res.send("<h1>Soham Express</h1>")
 })
 
-app.listen(3000, ()=>{})
+app.listen(process.env.PORT, ()=>{})
