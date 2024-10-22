0656e5a9c0dab97d993c60a26e5bd752be86ef76
sohamhalder <haldersoham46@gmail.com>
2024-10-23 03:16:16 +0530

    Aurora First Day Project Set Up

---
+++ web/src/app/App.tsx
@@ -0,0 +1,15 @@
+function App() {  
+
+  return (
+    <div className={"bg-blue-400 flex"} >
+      <div className={"bg-slate-400"} >
+      Soham
+    </div>
+    <div className={"bg-slate-900"} >
+      Soham
+    </div>
+    </div>
+  )
+}
+
+export default App
